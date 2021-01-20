b68c5e848e67e2b1608a78c1f38ccc3162700402
lugeminai <70250636+lugeminai@users.noreply.github.com>
2021-01-20 15:04:11 -0500

    Update app.module.ts
    vamos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ContactusComponent } from './pages/contactus/contactus.component';
+import { TeamComponent } from './pages/team/team.component';
+import { AboutusComponent } from './pages/aboutus/aboutus.component';
+import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContactusComponent,
+    TeamComponent,
+    AboutusComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule
