45a5ed06cade7568aa2b9388d82d77e95a4a86d6
lugeminai <70250636+lugeminai@users.noreply.github.com>
2021-01-23 19:33:29 -0500

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,24 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+import { ProjectComponent } from './pages/project/project.component';
+import { ApartmentComponent } from './pages/apartment/apartment.component';
 import { ContactusComponent } from './pages/contactus/contactus.component';
 import { AboutusComponent } from './pages/aboutus/aboutus.component';
 import { TeamComponent } from './pages/team/team.component';
-import {HomeComponent } from './pages/home/home.component';
-import { ProjectComponent } from './pages/project/project.component';
+import { HomeComponent } from './pages/home/home.component';
 
 const routes: Routes = [
-    { path: 'project', component: ProjectComponent},
-    { path: 'contactus', component: ContactusComponent },
-    { path: 'aboutus', component: AboutusComponent },
-    { path: 'team', component: TeamComponent },
-    { path: 'home', component: HomeComponent },
-    { path: '', redirectTo: 'home', pathMatch: 'full'} 
+  { path: 'project', component: ProjectComponent },
+  { path: 'apartment/:projectId', component: ApartmentComponent },
+  { path: 'contactus', component: ContactusComponent },
+  { path: 'aboutus', component: AboutusComponent },
+  { path: 'team', component: TeamComponent },
+  { path: 'home', component: HomeComponent },
+  { path: '', redirectTo: 'home', pathMatch: 'full' }
 ];
 
 @NgModule({
-  imports: [ RouterModule.forRoot(routes) ],  
+  imports: [ RouterModule.forRoot(routes) ],
   exports: [ RouterModule ]
 })
-
 export class AppRoutingModule {}
