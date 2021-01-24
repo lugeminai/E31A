0fa800cf2c63acbdf8028be6d296d217ec47c1f4
lugeminai <70250636+lugeminai@users.noreply.github.com>
2021-01-23 19:35:22 -0500

    Create project.service.ts

---
+++ src/app/services/project.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProjectService {
+
+  constructor(private readonly http: HttpClient) { }
+  
+  getProject(){
+    return this.http.get('/api/project/GetProject');
+  }
+}
