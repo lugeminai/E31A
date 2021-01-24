66fe6b723eb534fc346015b2a64d658cdf569077
lugeminai <70250636+lugeminai@users.noreply.github.com>
2021-01-24 15:48:16 -0500

    Update general.route.js

--- server/routes/general.route.js
+++ server/routes/general.route.js
@@ -1,6 +1,10 @@
 module.exports = app => {
-    app.get('/api/project/GetProject', (req, res) => {
+    app.get('/api/project/GetProjects', (req, res) => {
         var data = require('../json/project.json');
+        res.json(data);
+    })
+    app.get('/api/apartment/GetApartments', (req, res) => {
+        var data = require('../json/apartment.json');
         res.json(data);
     })
 }
