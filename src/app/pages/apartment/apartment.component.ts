537b12d90076bf551c87471cc402566f11d38da0
lugeminai <70250636+lugeminai@users.noreply.github.com>
2021-01-26 19:01:39 -0500

    Update apartment.component.ts

--- src/app/pages/apartment/apartment.component.ts
+++ src/app/pages/apartment/apartment.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { ApartmentService } from 'src/app/services/apartment.service';
+import { ProjectService } from 'src/app/services/project.service';
+
 
 @Component({
   selector: 'app-apartment',
@@ -7,9 +11,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ApartmentComponent implements OnInit {
 
-  constructor() { }
+  apartment = {projectId: 1};
+  project = {name: ""};
 
-  ngOnInit(): void {
+  constructor(private readonly apartmentService: ApartmentService, private readonly projectService:ProjectService, private activateRoute: ActivatedRoute) { }
+
+  getApartmentById(id: number){
+    this.apartmentService.getApartments().subscribe((rest: any) => {
+      this.apartment = rest.data.filter((item: { id: number; }) => item.id == id);
+      console.log(this.apartment);
+    })
   }
 
+  getProjectById(id: number){
+    this.projectService.getProjects().subscribe((rest: any) => {
+      this.project = rest.data.filter((item: { id: number; }) => item.id == id);
+      console.log(this.project);
+    })
+  }
+
+  ngOnInit(): void {
+    this.activateRoute.params.subscribe((params: Params) => {
+      if (params.id){
+        this.getApartmentById(params.id);
+        this.getProjectById(this.apartment.projectId);
+      }
+    });
+  }
 }
