f29d5eb58b3113640f2aeb09a72b4900ad0b1989
lugeminai <70250636+lugeminai@users.noreply.github.com>
2021-01-26 19:01:36 -0500

    Update contactus.component.ts

--- src/app/pages/contactus/contactus.component.ts
+++ src/app/pages/contactus/contactus.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contactus',
@@ -7,9 +8,48 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactusComponent implements OnInit {
 
-  constructor() { }
+  // contactusForm = new FormGroup({
+  //   person: new FormGroup({
+  //     firstname: new FormControl(''),
+  //     lastname: new FormControl('')
+  //   }),
+    
+  //   email: new FormControl(''),
+  //   address: new FormControl(''),
+  //   gender: new FormControl('')
+  // });
 
-  ngOnInit(): void {
+  contactusForm = this.fb.group({
+    person: this.fb.group({
+      firstname: ['', Validators.required],
+      lastname: ['', Validators.required]
+    }),
+    email: ['', Validators.required],
+    address: [''],
+    gender: ['']
+  });
+
+  constructor(private fb: FormBuilder) { }
+
+  updateValues(){
+    this.contactusForm.patchValue({
+      person: {
+        firstname: "James",
+        lastname: "Huiza"
+      },
+      email: "james.huiza@dominio.com.pe",
+      address: "Lima, Lima, Lima 17",
+      gender: "M"
+    });
   }
 
+  cleanValues(){
+    this.contactusForm.reset();
+  }
+
+  onSubmit(){
+    console.log(this.contactusForm.value)
+  }
+
+  ngOnInit(): void {   }
 }
